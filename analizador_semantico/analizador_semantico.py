430776669bee9187cf39e29214b66a23e43f45d2
floresjdfr <49382573+floresjdfr@users.noreply.github.com>
2020-11-07 08:38:59 -0600

    Analizador semantico
    -Verifica errores en condicionales

--- analizador_semantico/analizador_semantico.py
+++ analizador_semantico/analizador_semantico.py
@@ -29,35 +29,80 @@ class AnalisadorSemantico:
         
     def _analisis_identificadores(self, tabla):
         interes = {'int', 'string', 'float', 'identificador'}
-        operador = {'+', '-'}
         tabla = self._obtener_tabla()
         #x = y
         #(x, identificador), (=, asignacion), (y, identificador),(+, operador), (5, int)
         for tokens in self._lista_tokens:
             resultado = self._analizador_elementos(tokens)
+
             if resultado == 'ASIGNACION':
-                llave = tabla.buscar_simbolo(tokens[0][0])
+                llave = tabla.buscar_simbolo(tokens[0][0]) #verifica que la variable que esta a la izquierda del 
+                                                            #operador de asignacion este en la tabla
                 if llave:
                     lista_asignaciones = self._extraer_asignaciones(tokens)
                     #(y: identicador), (5, int)
                     for asignacion in lista_asignaciones:
                         if asignacion[1] == 'identificador':
-                            valor = tabla.buscar_simbolo(asignacion[0])
+                            valor = tabla.buscar_simbolo(asignacion[0]) #se verifica que la variable que va a ser asignada este en la tabla
                             if valor:
-                                if not valor['tipo'] == llave['tipo']:
+                                if not valor['tipo'] == llave['tipo']: #si la variable esta en la tabla se verifica que sea del mismo tipo
                                     print("Error")
-                        else:
+                        else: #si no es una variable es un tipo dato primitivo
                             tipo_valor = self._tipo_dato_checker(llave['tipo'], asignacion[0])
                             if not tipo_valor:
                                 print("Error")
                 else:
                     print("Error")
+            elif resultado == 'CONDICIONAL':
+                lista_asignaciones = self._extraer_asignaciones(tokens)
+                if len(lista_asignaciones) > 1: #caso en que hayan dos elementos comparados
+                    if lista_asignaciones[0][1] == 'identificador' and lista_asignaciones[1][1] == 'identificador': #caso de que ambos elementos sean variables
+                        elemento1 = tabla.buscar_simbolo(lista_asignaciones[0][0])
+                        elemento2 = tabla.buscar_simbolo(lista_asignaciones[1][0])
+                        if not elemento1 or not elemento2:
+                            if not elemento1:
+                                print('Error')
+                            else:
+                                print('Error')
+                    elif lista_asignaciones[0][1] == 'identificador' or lista_asignaciones[1][1] == 'identificador': #caso de que solo haya una variable y el otro sea algun tipo de dato primitivo
+                        elemento1 = lista_asignaciones[0]
+                        elemento2 = lista_asignaciones[1]
+                        if elemento1[1] == 'identificador':
+                            encontrado = tabla.buscar_simbolo(elemento1[0])
+                            if encontrado:
+                                if not elemento2[1] == encontrado['tipo']:
+                                    print('Error')
+                            else:
+                                print("Error")
+                        else:
+                            encontrado = tabla.buscar_simbolo(elemento1[0])
+                            if encontrado:
+                                if not elemento1[1] == encontrado['tipo']:
+                                    print('Error')
+                            else:
+                                print('Error')
+                    else:
+                        elemento1 = lista_asignaciones[0]
+                        elemento2 = lista_asignaciones[1]
+                        if not elemento1[1] == elemento2[2]:#caso de que los datos primitivos no sean del mismo tipo
+                            print("Error")
+                else: #en caso de que el condicional solo tenga un elemento dentro
+                    if lista_asignaciones[0][1] == 'identificador':#caso de que ambos elementos sean variables
+                        elemento1 = tabla.buscar_simbolo(lista_asignaciones[0][0])
+                        if not elemento1:
+                            print('Error')
+                    else: #puede ser innecesario
+                        elemento1 = lista_asignaciones[0]
+                        encontrado = tabla.buscar_simbolo(elemento1[0])
+                        if not encontrado:
+                            print('Error')
+
 
     def _extraer_asignaciones(self, linea_token):
         #[(x, identificador), (=, asignacion),(y, identificador),(+, operador_aritmentico), (z, identificador),(-, operador-aritmentico), (5, int)]
         interes = {'identificador', 'int', 'string', 'float'}
         lista_asignaciones = []
-        for i in range(2, len(linea_token)+1):
+        for i in range(0, len(linea_token)+1):
             if linea_token[i][1] in interes:
                 lista_asignaciones = linea_token[i]
         return lista_asignaciones
